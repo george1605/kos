f3a2b9f05b55d0f8f5d735ef5e5490f67ed6fd2b
george1605 <32997165+george1605@users.noreply.github.com>
2022-07-17 13:19:52 +0300

    Create ramdisk.h

---
+++ drivers/ramdisk.h
@@ -0,0 +1,28 @@
+/***
+RamDisk - an alternative to Physical Drives
+***/
+#include "../smem.h"
+#include "../vfs.h"
+
+struct ramdisk
+{
+  void* ptr;
+  size_t size;
+  int fd;
+  int prot;
+};
+
+void raminit(struct ramdisk* i)
+{
+  if(i == NULL_PTR)
+    return;
+  i->size = 4096;
+  i->ptr = alloc(0, 4096);
+  i->prot = M_BOTH;
+}
+
+void rammount(struct ramdisk* i, char* x)
+{
+  struct vfile n = vfsmap(x, i->ptr);
+  i->fd = n.fd;
+}
