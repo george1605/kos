4e14cc62ead00a2fe703e6cdc52f2cb1ab98129f
george1605 <george160505@gmail.com>
2022-07-18 15:54:58 +0300

    added terminal & stack functions

--- noarch.h
+++ noarch.h
@@ -4,6 +4,8 @@
 #define __DIFF(a, b) (int)(&b - &a)
 #define __ENDCHR '\0'
 #define __RETCHR '\r'
+#define __INVPTR (void*)-1 
+#define __FGWHITE 0xF
 
 struct refptr
 {
@@ -29,6 +31,15 @@ struct termis
   void(*handler)(struct termis* p, char c); // handler for special chars
 };
 
+static int stack_check()
+{
+  int a = 0;
+  int b = 0;
+  if(__DIFF(a,b) != sizeof(int))
+    return 1;
+  return 0;
+}
+
 static void termis_handler(struct termis* p, char c)
 {
   switch(c)
@@ -41,4 +52,24 @@ static void termis_handler(struct termis* p, char c)
     p->pos.y++;
     break;
   }
+}
+
+static void termis_write(struct termis* p, const char* f)
+{
+  int a;
+  while(a = 0;f[a] != 0;a++)
+  {
+    if(f[a] == '\0' || f[a] == '\n' || f[a] == '\r')
+      p->handler(p, f[a]);
+    (char*)p->buffer[a] = f[a];
+  }
+}
+
+static void termis_init(struct termis* p)
+{
+  if(p == 0 || p == __INVPTR)
+    return;
+  p->buffer = 0x1000;
+  p->flags = __FGWHITE;
+  p->handler = termis_handler;
 }
