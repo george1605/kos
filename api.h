ac105a8a806beaf4e06fd75c401e26142a013884
george1605 <george160505@gmail.com>
2022-07-18 13:24:23 +0300

    added inb/outb to the api function list

--- api.h
+++ api.h
@@ -18,11 +18,29 @@ void* __protexec(void* pr) // kinda unsafe
   protexec(*(struct proc*)pr);
 }
 
+void* __inb(void* args)
+{
+  if(args == NULL_PTR)
+    return;
+
+  return (void*)inb(*(uint16_t*)port);
+}
+
+void* __outb(void* args)
+{
+  short* p = (short*)args;
+  char* r = (char*)(args + 2);
+  outb(p, r);
+  return NULL;
+}
+
 void api_init()
 {
   api_list = alloc(0,sizeof(apifunc) * 16);
   api_list[0] = __procnew;
   api_list[1] = __protexec;
+  api_list[2] = __inb;
+  api_list[3] = __outb;
 }
 
 void api_call(int num, void* args)
--- noarch.h
+++ noarch.h
@@ -1,7 +1,44 @@
+/***
+ Architecture-independent stuff
+***/
 #define __DIFF(a, b) (int)(&b - &a)
+#define __ENDCHR '\0'
+#define __RETCHR '\r'
 
 struct refptr
 {
   void* ptr;
   int count;
 };
+
+struct file;
+struct vfile;
+struct pin;
+
+struct chrpos
+{
+  int x, y;
+};
+
+struct termis
+{
+  void* buffer;
+  int fd;
+  int flags; 
+  chrpos pos;
+  void(*handler)(struct termis* p, char c); // handler for special chars
+};
+
+static void termis_handler(struct termis* p, char c)
+{
+  switch(c)
+  {
+    case __ENDCHR:
+    break;
+
+    case __RETCHR:
+    p->pos.x = 0;
+    p->pos.y++;
+    break;
+  }
+}
